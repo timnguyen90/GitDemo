3b0d6f1c00dbd08df1626567cbdce7ce8e198483
timnguyen90 <33167258+timnguyen90@users.noreply.github.com>
2018-07-16 02:10:17 +0700

    Fixed issue53 in Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,9 +3,15 @@ public public class Student
   	public int Id { get; set; }
   	public string FullName { get; set; }
   	
-  	public Student (Parameters) 
+  	public Student () 
   	{
   	  
   	}
+	//Fix issue
+	public Student(int Id, string FullName){
+		this.Id = ID;
+		this.FullName = FullName;
+	}
+	//End Fix issue
 
 }
