7e21c549eefd1549bd3ac55dd128dc66f1a13393
timnguyen90 <33167258+timnguyen90@users.noreply.github.com>
2018-07-15 22:40:33 +0700

    Initial Models

---
+++ Models/Teacher.cs
@@ -0,0 +1,11 @@
+public public class Teacher 
+{
+	public int Id { get; set; }
+  	public string FullName { get; set; }
+  	public int DepId { get; set; }
+
+  	public Teacher () 
+  	{
+  	  
+  	}
+}
